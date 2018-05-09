7b6039969d1334c3ee7866f4ba5a2fd81ca80d82
MarkLyck <mark.lyck@gmail.com>
2018-05-09 20:37:36 +0200

    Gift icon, and pricing

--- src/common/utils/fontAwesomeLibrary.js
+++ src/common/utils/fontAwesomeLibrary.js
@@ -2,5 +2,11 @@ import fontawesome from '@fortawesome/fontawesome'
 import faSignInAlt from '@fortawesome/fontawesome-free-solid/faSignInAlt'
 import faSignOutAlt from '@fortawesome/fontawesome-free-solid/faSignOutAlt'
 import faChartLine from '@fortawesome/fontawesome-free-solid/faChartLine'
+import faGift from '@fortawesome/fontawesome-free-solid/faGift'
 
-fontawesome.library.add(faSignInAlt, faSignOutAlt, faChartLine)
+fontawesome.library.add(
+    faSignInAlt,
+    faSignOutAlt,
+    faChartLine,
+    faGift,
+)
--- src/pages/Retail/06_FirstMonthOnUs/FirstMonthOnUs.jsx
+++ src/pages/Retail/06_FirstMonthOnUs/FirstMonthOnUs.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { css } from 'emotion'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import Section from 'components/Section'
 import SectionTitle from 'components/Section/SectionTitle'
 import Subtitle from 'components/Section/Subtitle'
 import Button from 'components/Button'
+import { PricingText } from './styles'
 
 const FirstMonthOnus = () => (
     <Section>
         <SectionTitle>First month is on us</SectionTitle>
-        <Subtitle>If you decide Formula Stocks isn't for you - simply cancel online at any time without obligations. After the first month the service costs <span className={css`text-decoration: underline`}>$50 a month</span></Subtitle>
-        <Button variant="raised" size="large">Start your free month</Button>
+        <Subtitle>If you decide Formula Stocks isn't for you - simply cancel online at any time without obligations.</Subtitle>
+        <PricingText>$50 monthly after your <span className="underline">free 30 days</span></PricingText>
+        <Button variant="raised" size="large"><FontAwesomeIcon icon="gift" />Start your free month</Button>
     </Section>
 )
 
