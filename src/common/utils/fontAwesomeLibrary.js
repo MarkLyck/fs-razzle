abd282fc921b58ad01bfffd0174bef63a7eb6000
MarkLyck <mark.lyck@gmail.com>
2018-05-18 12:31:22 +0200

    all aliases for icons

--- src/common/utils/fontAwesomeLibrary.js
+++ src/common/utils/fontAwesomeLibrary.js
@@ -1,38 +1,40 @@
 import fontawesome from '@fortawesome/fontawesome'
 import faSignInAlt from '@fortawesome/fontawesome-pro-solid/faSignInAlt'
-// import faSignOutAlt from '@fortawesome/fontawesome-pro-solid/faSignOutAlt'
-// import faChartLine from '@fortawesome/fontawesome-pro-solid/faChartLine'
-// import faChartPie from '@fortawesome/fontawesome-pro-solid/faChartPie'
-// import faFlask from '@fortawesome/fontawesome-pro-solid/faFlask'
-// import faGift from '@fortawesome/fontawesome-pro-solid/faGift'
-// import faTimes from '@fortawesome/fontawesome-pro-regular/faTimes'
-// import faTasks from '@fortawesome/fontawesome-pro-solid/faTasks'
-// import faListUl from '@fortawesome/fontawesome-pro-solid/faListUl'
-// import faNewsPaper from '@fortawesome/fontawesome-pro-solid/faNewsPaper'
-// import faTachoMeter from '@fortawesome/fontawesome-pro-solid/faTachoMeter'
-// import faUser from '@fortawesome/fontawesome-pro-solid/faUser'
-// import faQuestionCircle from '@fortawesome/fontawesome-pro-solid/faQuestionCircle'
-// import faDollarSign from '@fortawesome/fontawesome-pro-solid/faDollarSign'
-// import faAngleDown from '@fortawesome/fontawesome-pro-solid/faAngleDown'
-// import faExclamationCircle from '@fortawesome/fontawesome-pro-solid/faExclamationCircle'
-// import faSpinnerThird from '@fortawesome/fontawesome-pro-solid/faSpinnerThird'
+import faSignOutAlt from '@fortawesome/fontawesome-pro-solid/faSignOutAlt'
+import faChartLine from '@fortawesome/fontawesome-pro-solid/faChartLine'
+import faChartPie from '@fortawesome/fontawesome-pro-solid/faChartPie'
+import faFlask from '@fortawesome/fontawesome-pro-solid/faFlask'
+import faGift from '@fortawesome/fontawesome-pro-solid/faGift'
+import faTasks from '@fortawesome/fontawesome-pro-solid/faTasks'
+import faListUl from '@fortawesome/fontawesome-pro-solid/faListUl'
+import faNewsPaper from '@fortawesome/fontawesome-pro-solid/faNewsPaper'
+import faTachoMeter from '@fortawesome/fontawesome-pro-solid/faTachoMeter'
+import faUser from '@fortawesome/fontawesome-pro-solid/faUser'
+import faQuestionCircle from '@fortawesome/fontawesome-pro-solid/faQuestionCircle'
+import faDollarSign from '@fortawesome/fontawesome-pro-solid/faDollarSign'
+import faAngleDown from '@fortawesome/fontawesome-pro-solid/faAngleDown'
+import faExclamationCircle from '@fortawesome/fontawesome-pro-solid/faExclamationCircle'
+import faSpinnerThird from '@fortawesome/fontawesome-pro-solid/faSpinnerThird'
+
+import faTimes from '@fortawesome/fontawesome-pro-regular/faTimes'
+
 
 fontawesome.library.add(
     faSignInAlt,
-    // faSignOutAlt,
-    // faFlask,
-    // faChartLine,
-    // faChartPie,
-    // faGift,
-    // faTimes,
-    // faTasks,
-    // faListUl,
-    // faNewsPaper,
-    // faTachoMeter,
-    // faUser,
-    // faQuestionCircle,
-    // faDollarSign,
-    // faAngleDown,
-    // faExclamationCircle,
-    // faSpinnerThird,
+    faSignOutAlt,
+    faFlask,
+    faChartLine,
+    faChartPie,
+    faGift,
+    faTimes,
+    faTasks,
+    faListUl,
+    faNewsPaper,
+    faTachoMeter,
+    faUser,
+    faQuestionCircle,
+    faDollarSign,
+    faAngleDown,
+    faExclamationCircle,
+    faSpinnerThird,
 )
