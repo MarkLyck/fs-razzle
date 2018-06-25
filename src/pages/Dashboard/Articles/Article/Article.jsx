adc1ad1671a9438a3d271a6160154008688c7001
MarkLyck <mark.lyck@gmail.com>
2018-06-25 06:25:03 +0200

    Article fetch

--- src/pages/Dashboard/Articles/Article/Article.jsx
+++ src/pages/Dashboard/Articles/Article/Article.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import GenericLoader from 'components/Loading/Generic'
+import LoadingError from 'components/Error/LoadingError'
 import withDashboard from 'components/withDashboard'
-import { HeaderImage, Title, Body } from './styles'
+import { ArticleContainer, HeaderImage, Title, Body } from './styles'
 
 const ARTICLE_QUERY = gql`
   query ArticleQuery($title: String) {
@@ -14,23 +16,28 @@ const ARTICLE_QUERY = gql`
   }
 `
 
-const Article = () => (
-  <Query query={ARTICLE_QUERY} variables={{ title: 'test' }}>
-    {({ loading, error, data }) => {
-      if (loading) return <div>loading</div>
-      if (error) return <div>Error</div>
+const Article = ({ location }) => {
+  const locationArr = location.pathname.split('/')
+  const articleTitle = locationArr[locationArr.length - 1].split('-').join(' ')
 
-      const article = data.allArticles[0]
+  return (
+    <Query query={ARTICLE_QUERY} variables={{ title: articleTitle }}>
+      {({ loading, error, data }) => {
+        if (loading) return <GenericLoader />
+        if (error) return <LoadingError />
 
-      return (
-        <div>
-          <HeaderImage data-headerimageurl={article.headerImageUrl} />
-          <Title>{article.title}</Title>
-          <Body dangerouslySetInnerHTML={{ __html: article.body }} />
-        </div>
-      )
-    }}
-  </Query>
-)
+        const article = data.allArticles[0]
+
+        return (
+          <ArticleContainer>
+            <HeaderImage data-headerimageurl={article.headerImageUrl} />
+            <Title>{article.title}</Title>
+            <Body dangerouslySetInnerHTML={{ __html: article.body }} />
+          </ArticleContainer>
+        )
+      }}
+    </Query>
+  )
+}
 
 export default withDashboard(Article)
--- src/pages/Dashboard/Articles/Article/styles.js
+++ src/pages/Dashboard/Articles/Article/styles.js
@@ -1,5 +1,11 @@
 import styled from 'react-emotion'
 
+export const ArticleContainer = styled.div`
+  background: white;
+  width: 100%;
+  height: 100%;
+`
+
 export const HeaderImage = styled.div`
   width: 100%;
   height: 400px;
@@ -10,12 +16,16 @@ export const HeaderImage = styled.div`
 
 export const Title = styled.h1`
   padding: 16px 32px;
-  margin: 0;
-  font-weight: 500;
+  margin: 16px auto 0;
+  width: 100%;
+  max-width: 800px;
+  font-weight: bold;
+  font-size: 1.8rem;
 `
 
 export const Body = styled.div`
   padding: 0 32px;
   width: 100%;
   max-width: 800px;
+  margin: 0 auto;
 `
