c33420c23810d0d1304cfe2ed96567ac7177f5e3
MarkLyck <mark.lyck@gmail.com>
2018-06-26 07:31:43 +0200

    reuseable article components

--- src/pages/Articles/Article/Article.jsx
+++ src/pages/Articles/Article/Article.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import withDashboard from 'components/withDashboard'
 import ArticlePage from 'components/Articles/Article'
 
 const Article = ({ location }) => <ArticlePage location={location} />
 
-export default withDashboard(Article)
+export default Article
--- src/pages/Articles/Articles.jsx
+++ src/pages/Articles/Articles.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import withDashboard from 'components/withDashboard'
 import ArticlesList from 'components/Articles'
 
 const Articles = ({ location }) => <ArticlesList location={location} />
 
-export default withDashboard(Articles)
+export default Articles
