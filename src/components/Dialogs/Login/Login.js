235469014886091b8dc6ac7b29334f9850b68c25
MarkLyck <mark.lyck@gmail.com>
2018-09-10 16:32:06 +0200

    forgot password styles

--- src/components/Dialogs/Login/Login.js
+++ src/components/Dialogs/Login/Login.js
@@ -5,6 +5,7 @@ import { graphql } from 'react-apollo'
 import Modal from 'react-modal'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ModalContainer, overlayClass, modalStyles } from '../styles'
+import { ForgotPassword } from './styles'
 import { Formik } from 'formik'
 import Form, { Row, Field, ErrorMessage } from 'components/Form'
 import ModalHeader from 'components/Dialogs/ModalHeader'
@@ -126,7 +127,7 @@ class Login extends Component {
                         'Login'
                       )}
                     </Button>
-                    <button onClick={this.toggleResetPassword}>Forgot your password?</button>
+                    <ForgotPassword onClick={this.toggleResetPassword}>Forgot your password?</ForgotPassword>
                   </Form>
                 )}
               />
---
+++ src/components/Dialogs/Login/styles.js
@@ -0,0 +1,6 @@
+import styled from 'react-emotion'
+
+export const ForgotPassword = styled.a`
+  margin-top: 16px;
+  font-size: 0.8rem;
+`
