9d98a273d9bcdf3bac2167ed96889d2bd5644992
MarkLyck <mark.lyck@gmail.com>
2018-09-16 00:23:59 +0200

    Fix portfolio graph DJIA color

--- src/pages/Dashboard/Portfolio/PortfolioHeader/PortfolioChart/styles.js
+++ src/pages/Dashboard/Portfolio/PortfolioHeader/PortfolioChart/styles.js
@@ -1,30 +1,34 @@
 import styled from 'react-emotion'
 
 export const GraphContainer = styled.div`
-    position: relative;
-    width: 100%;
-    height: 360px;
-    br {
-        display: none;
+  position: relative;
+  width: 100%;
+  height: 360px;
+  br {
+    display: none;
+  }
+  .portfolio-legends {
+    top: 24px;
+  }
+  #portfolio-graph {
+    height: 100%;
+  }
+  .chart-balloon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .plan-name {
+      color: ${props => props.theme.colors.primary};
+      text-tranform: uppercase
+      font-weight: bold;
+      font-size: 0.8rem;
     }
-    .portfolio-legends {
-        top: 24px;
+    .market-name {
+      color: ${props => props.theme.colors.black};
     }
-    #portfolio-graph {
-        height: 100%;
+    .balloon-value {
+      font-size: 1rem;
+      text-align: center;
     }
-    .chart-balloon {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        .plan-name {
-            color: ${props => props.theme.colors.primary};
-            font-size: 0.8rem;
-        }
-        .balloon-value {
-            font-size: 1rem;
-            text-align: center;
-        }
-    }
+  }
 `
-
