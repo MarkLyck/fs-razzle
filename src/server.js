61b753868397ff78917c1a10e964aa0b981db30c
MarkLyck <mark.lyck@gmail.com>
2018-09-15 17:02:33 +0200

    add hotjar and google analytics

--- src/server.js
+++ src/server.js
@@ -54,39 +54,40 @@ server
             </head>
             <body>
                 <div id="root">${markup}</div>
+
                 <!-- Intercom -->
                 <script>
-                  (function() {
-                    var w=window;
-                    var ic=w.Intercom;
-                    if(typeof ic==="function"){
-                      ic('reattach_activator');ic('update',intercomSettings);
-                    }
-                    else{
-                      var d=document;var i=function(){
-                        i.c(arguments)
-                      };
-                      i.q=[];
-                      i.c=function(args){
-                        i.q.push(args)
-                      };
-                      w.Intercom=i;
-                      function l(){
-                        var s=d.createElement('script');
-                        s.type='text/javascript';
-                        s.async=true;
-                        s.src='https://widget.intercom.io/widget/i194mpvo';
-                        var x=d.getElementsByTagName('script')[0];
-                        x.parentNode.insertBefore(s,x);
-                      }
-                      if(w.attachEvent){
-                        w.attachEvent('onload',l);
-                      }
-                      else{
-                        w.addEventListener('load',l,false);
-                      }}
-                  })()
-                </script>  
+                  (function() {var w=window;var ic=w.Intercom;
+                  if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{
+                  var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};
+                  w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;
+                  s.src='https://widget.intercom.io/widget/i194mpvo';var x=d.getElementsByTagName('script')[0];
+                  x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{
+                  w.addEventListener('load',l,false);}}})()
+                </script>
+
+                <!-- Hotjar -->
+                <script>
+                  (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:399997,hjsv:5};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                  })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+                </script>
+
+                <!-- Google Analytics -->
+                <script>
+                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                  ga('create', 'UA-68151102-1', 'auto');
+                  ga('send', 'pageview');
+                </script>
             </body>
         </html>`
       )
