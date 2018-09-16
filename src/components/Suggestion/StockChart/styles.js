610ba6dd2e124e00720c705432bd2051ce750886
MarkLyck <mark.lyck@gmail.com>
2018-09-16 02:45:43 +0200

    Risk management

--- src/components/Suggestion/StockChart/styles.js
+++ src/components/Suggestion/StockChart/styles.js
@@ -2,66 +2,65 @@ import { css } from 'emotion'
 import styled from 'react-emotion'
 
 const altStyle = css`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    svg {
-        font-size: 2rem;
-    }
-    h4 {
-        margin-top: 16px;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  svg {
+    font-size: 2rem;
+  }
+  h4 {
+    margin-top: 16px;
+  }
 `
 
 export const LoadingContainer = styled.div`
-    ${altStyle}
-    svg {
-        font-size: 3rem;
-        color: ${props => props.theme.colors.primary};
-    }
-    h4 {
-        color: ${props => props.theme.colors.black};
-    }
+  ${altStyle} svg {
+    font-size: 3rem;
+    color: ${props => props.theme.colors.primary};
+  }
+  h4 {
+    color: ${props => props.theme.colors.black};
+  }
 `
 
 export const FailedContainer = styled.div`
-    ${altStyle}
-    margin: 16px;
-    color: ${props => props.theme.colors.black};
-    border: 2px dashed ${props => props.theme.colors.lightGray};
+  ${altStyle} margin: 16px;
+  color: ${props => props.theme.colors.black};
+  border: 2px dashed ${props => props.theme.colors.lightGray};
+  text-align: center;
 `
 
 export const GraphContainer = styled.div`
-    position: relative;
-    width: 100%;
-    height: auto;
-    .suggestion-graph {
-        height: 100%;
+  position: relative;
+  width: 100%;
+  height: auto;
+  .suggestion-graph {
+    height: 100%;
+  }
+  br {
+    display: none;
+  }
+  .chart-balloon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .ticker-name {
+      color: ${props => props.theme.colors.primary};
+      font-size: 0.8rem;
+      margin: 0;
+      padding: 0;
     }
-    br {
-        display: none;
+    .SELL-ticker-name {
+      color: ${props => props.theme.colors.secondary};
     }
-    .chart-balloon {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        .ticker-name {
-            color: ${props => props.theme.colors.primary};
-            font-size: 0.8rem;
-            margin: 0;
-            padding: 0;
-        }
-        .SELL-ticker-name {
-            color: ${props => props.theme.colors.secondary};
-        }
-        .balloon-value {
-            font-size: 1rem;
-            text-align: center;
-            margin: 0;
-            padding: 0;
-        }
+    .balloon-value {
+      font-size: 1rem;
+      text-align: center;
+      margin: 0;
+      padding: 0;
     }
+  }
 `
