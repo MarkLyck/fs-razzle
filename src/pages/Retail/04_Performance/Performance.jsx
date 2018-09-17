b1bad2be450ac8a2a2b015655af7fd7dea523cf9
MarkLyck <mark.lyck@gmail.com>
2018-09-17 21:41:06 +0200

    improve first load performance, fix integration tests

--- src/pages/Retail/04_Performance/Performance.jsx
+++ src/pages/Retail/04_Performance/Performance.jsx
@@ -1,23 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 import { Element } from 'react-scroll'
+import { marketIds } from 'common/constants'
 import Section from 'components/Section'
 import SectionTitle from 'components/Section/SectionTitle'
 import Subtitle from 'components/Section/Subtitle'
 import LaunchPerformance from './LaunchPerformance'
 
-const Performance = ({ portfolioYields, marketPrices, planName, amChartsLoaded }) => (
-  <Section>
-    <Element name="performance" />
-    <SectionTitle>Performance</SectionTitle>
-    <Subtitle>Unleveraged returns since 2009, compared to the Dow Jones Industrial Average.</Subtitle>
-    <LaunchPerformance
-      portfolioYields={portfolioYields}
-      marketPrices={marketPrices}
-      planName={planName}
-      amChartsLoaded={amChartsLoaded}
-    />
-  </Section>
+const GET_DATA_SINCE_2009 = gql`
+  query {
+    DJIA: Market(id: "${marketIds.DJIA}") {
+      pricesSince2009
+    }
+  }
+`
+
+const Performance = ({ portfolioYields, planName, amChartsLoaded }) => (
+  <Query query={GET_DATA_SINCE_2009}>
+    {({ loading, error, data }) => {
+      const { DJIA = {} } = data
+      return (
+        <Section>
+          <Element name="performance" />
+          <SectionTitle>Performance</SectionTitle>
+          <Subtitle>Unleveraged returns since 2009, compared to the Dow Jones Industrial Average.</Subtitle>
+          <LaunchPerformance
+            portfolioYields={portfolioYields}
+            marketPrices={DJIA.pricesSince2009 || []}
+            planName={planName}
+            amChartsLoaded={amChartsLoaded}
+          />
+        </Section>
+      )
+    }}
+  </Query>
 )
 
 Performance.propTypes = {
--- src/pages/Retail/Retail.jsx
+++ src/pages/Retail/Retail.jsx
@@ -3,9 +3,9 @@ import _ from 'lodash'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 import Script from 'react-load-script'
-import { planIds, marketIds } from 'common/constants'
+import { planIds } from 'common/constants'
 import { hasStorage, usingMocks } from 'common/utils/featureTests'
-import mockData from 'common/mocks/RetailData.json'
+// import mockData from 'common/mocks/RetailData.json'
 import HomeLoader from 'components/Loading/HomeLoader'
 import LoadingError from 'components/Error/LoadingError'
 import Navbar from 'components/Navbar/Retail'
@@ -28,38 +28,16 @@ import CorporateProfile from './13_CorporateProfile'
 import ScrolledToBottom from './15_ScrolledToBottom'
 import Footer from './16_Footer'
 
-// import History from './History'
-
-// const ARTICLES_QUERY = gql`
-//   query {
-//     allArticles {
-//         title
-//         body
-//         headerImageUrl
-//     }
-//   }
-// `
-
 const GET_ENTRY_AND_MARKET_DATA = gql`
   query {
     Plan(id: "${planIds.ENTRY}") {
       name
-      backtestedData
       info
       price
       portfolioYields
       launchStatistics
       latestSells
       statistics
-    },
-    SP500: Market(id: "${marketIds.SP500}") {
-        name
-        pricesSince2009
-        longtermPrices
-    }
-    DJIA: Market(id: "${marketIds.DJIA}") {
-        name
-        pricesSince2009
     }
   }
 `
@@ -113,22 +91,19 @@ class Retail extends Component {
       <Query query={GET_ENTRY_AND_MARKET_DATA}>
         {({ loading, error, data }) => {
           if (loading) return <HomeLoader />
-          if (error && !usingMocks) return <LoadingError />
+          if (!data || !data.Plan || (error && !usingMocks)) return <LoadingError />
 
-          const plan = data ? data.Plan : mockData.Plan
-          const DJIA = data ? data.DJIA : mockData.DJIA
-          const SP500 = data ? data.SP500 : mockData.SP500
+          const { Plan } = data
 
-          const planName = _.get(plan, 'name')
-          const portfolioReturn = _.get(plan, 'launchStatistics.total_return')
-          const portfolioYields = _.get(plan, 'portfolioYields')
-          const backtestedData = _.get(plan, 'backtestedData')
-          const latestSells = _.get(plan, 'latestSells')
-          const winRatio = _.get(plan, 'statistics.winRatio')
-          const CAGR = _.get(plan, 'statistics.CAGR')
-          const avgGain = _.get(plan, 'info.avgGainPerPosition')
-          const avgLoss = _.get(plan, 'info.avgLossPerPosition')
-          const sortinoRatio = _.get(plan, 'info.sortinoRatio')
+          const planName = _.get(Plan, 'name')
+          const portfolioReturn = _.get(Plan, 'launchStatistics.total_return')
+          const portfolioYields = _.get(Plan, 'portfolioYields')
+          const latestSells = _.get(Plan, 'latestSells')
+          const winRatio = _.get(Plan, 'statistics.winRatio')
+          const CAGR = _.get(Plan, 'statistics.CAGR')
+          const avgGain = _.get(Plan, 'info.avgGainPerPosition')
+          const avgLoss = _.get(Plan, 'info.avgLossPerPosition')
+          const sortinoRatio = _.get(Plan, 'info.sortinoRatio')
 
           return (
             <div className="retail-page">
@@ -146,22 +121,12 @@ class Retail extends Component {
                 planName={planName}
               />
               <WhatIsIt />
-              <Performance
-                portfolioYields={portfolioYields}
-                marketPrices={DJIA.pricesSince2009}
-                planName={planName}
-                amChartsLoaded={amChartsLoaded}
-              />
+              <Performance portfolioYields={portfolioYields} planName={planName} amChartsLoaded={amChartsLoaded} />
               <PercentMatters portfolioReturn={portfolioReturn} CAGR={CAGR} />
               <FirstMonthOnUs toggleSignUpModal={this.toggleSignUpModal} />
               <WhatToExpect latestSells={latestSells} winRatio={winRatio} />
               <PilotProgram />
-              <LongTermPerformance
-                backtestedData={backtestedData}
-                marketPrices={SP500.longtermPrices}
-                planName={planName}
-                amChartsLoaded={amChartsLoaded}
-              />
+              <LongTermPerformance planName={planName} amChartsLoaded={amChartsLoaded} />
               <Statistics
                 winRatio={winRatio}
                 planName={planName}
@@ -184,7 +149,7 @@ class Retail extends Component {
                 </React.Fragment>
               ) : null}
               {signUpVisible && (
-                <Signup history={history} onRequestClose={this.toggleSignUpModal} planPrice={plan.price} />
+                <Signup history={history} onRequestClose={this.toggleSignUpModal} planPrice={Plan.price} />
               )}
               {loginVisible && <Login history={history} onRequestClose={this.toggleLoginModal} />}
               {FAQVisible && <FAQ history={history} hide={this.toggleFAQModal} />}
