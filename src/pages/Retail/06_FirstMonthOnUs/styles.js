80a019d2c8d6dfa43f8a64850dd51943a9f3c65e
MarkLyck <mark.lyck@gmail.com>
2018-10-06 11:05:57 +0200

    fix visitor bug in admin, improve look of 49.99

--- src/pages/Retail/06_FirstMonthOnUs/styles.js
+++ src/pages/Retail/06_FirstMonthOnUs/styles.js
@@ -1,11 +1,15 @@
 import styled from 'react-emotion'
 
 export const PricingText = styled.h3`
-    font-weight: bold;
-    text-align: center;
-    margin-bottom: 16px;
-    font-size: 1.5rem;
-    .underline {
-        text-decoration: underline;
-    }
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 16px;
+  font-size: 1.5rem;
+  .underline {
+    text-decoration: underline;
+  }
+
+  .decimals {
+    font-size: 0.9rem;
+  }
 `
